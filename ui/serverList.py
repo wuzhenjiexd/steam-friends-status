18999665aaf2197e33974e57ad0a0c927afb8bcc
davidk <kdavid@gmail.com>
2009-03-12 16:53:21 -0700

    Friends ID/Profiles detection/Server list info
    Profile detection:
    The entire steamcommunity URL to a profile can now be provided when
    querying through Steam Friends Status.
    
    Server list:
    Add sorting (which doesn't work as it should on some columns since
    it does string based sorting), and resize the columns to take up
    as much space as possible.

--- ui/serverList.py
+++ ui/serverList.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'serverList.ui'
 #
-# Created: Thu Mar 12 14:57:10 2009
+# Created: Thu Mar 12 15:26:11 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,13 +13,24 @@ class Ui_serverList(object):
     def setupUi(self, serverList):
         serverList.setObjectName("serverList")
         serverList.resize(428, 466)
+        serverList.setMaximumSize(QtCore.QSize(428, 466))
         self.serverInfoWidget = QtGui.QListWidget(serverList)
-        self.serverInfoWidget.setGeometry(QtCore.QRect(0, 10, 421, 231))
+        self.serverInfoWidget.setGeometry(QtCore.QRect(0, 10, 421, 191))
+        self.serverInfoWidget.setMaximumSize(QtCore.QSize(421, 191))
         self.serverInfoWidget.setObjectName("serverInfoWidget")
         self.tableScores = QtGui.QTableWidget(serverList)
-        self.tableScores.setGeometry(QtCore.QRect(0, 250, 421, 171))
+        self.tableScores.setGeometry(QtCore.QRect(0, 210, 421, 221))
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tableScores.sizePolicy().hasHeightForWidth())
+        self.tableScores.setSizePolicy(sizePolicy)
+        self.tableScores.setMaximumSize(QtCore.QSize(421, 221))
         self.tableScores.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.tableScores.setAlternatingRowColors(False)
+        self.tableScores.setAlternatingRowColors(True)
+        self.tableScores.setShowGrid(False)
+        self.tableScores.setWordWrap(False)
+        self.tableScores.setCornerButtonEnabled(False)
         self.tableScores.setRowCount(0)
         self.tableScores.setColumnCount(3)
         self.tableScores.setObjectName("tableScores")
@@ -31,16 +42,12 @@ class Ui_serverList(object):
         self.tableScores.setHorizontalHeaderItem(1, item)
         item = QtGui.QTableWidgetItem()
         self.tableScores.setHorizontalHeaderItem(2, item)
-        self.refreshButton = QtGui.QPushButton(serverList)
-        self.refreshButton.setGeometry(QtCore.QRect(240, 430, 80, 29))
-        self.refreshButton.setObjectName("refreshButton")
         self.closeButton = QtGui.QPushButton(serverList)
         self.closeButton.setGeometry(QtCore.QRect(330, 430, 80, 29))
         self.closeButton.setObjectName("closeButton")
 
         self.retranslateUi(serverList)
         QtCore.QObject.connect(self.closeButton, QtCore.SIGNAL("clicked()"), serverList.close)
-        QtCore.QObject.connect(self.refreshButton, QtCore.SIGNAL("clicked()"), serverList.update)
         QtCore.QMetaObject.connectSlotsByName(serverList)
 
     def retranslateUi(self, serverList):
@@ -49,6 +56,5 @@ class Ui_serverList(object):
         self.tableScores.horizontalHeaderItem(0).setText(QtGui.QApplication.translate("serverList", "Player", None, QtGui.QApplication.UnicodeUTF8))
         self.tableScores.horizontalHeaderItem(1).setText(QtGui.QApplication.translate("serverList", "Score", None, QtGui.QApplication.UnicodeUTF8))
         self.tableScores.horizontalHeaderItem(2).setText(QtGui.QApplication.translate("serverList", "Time", None, QtGui.QApplication.UnicodeUTF8))
-        self.refreshButton.setText(QtGui.QApplication.translate("serverList", "Refresh", None, QtGui.QApplication.UnicodeUTF8))
         self.closeButton.setText(QtGui.QApplication.translate("serverList", "Close", None, QtGui.QApplication.UnicodeUTF8))
 
