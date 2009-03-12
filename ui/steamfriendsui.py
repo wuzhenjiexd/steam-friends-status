fb12939cc047cfa8db479fd20d27454b07b982ef
davidk <kdavid@gmail.com>
2009-03-11 23:19:47 -0700

    UI refinement. Also add an error popup for a blank Steam Community ID/Alias.

--- ui/steamfriendsui.py
+++ ui/steamfriendsui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'steamfriends.ui'
 #
-# Created: Mon Dec  1 00:30:10 2008
+# Created: Wed Mar 11 23:15:19 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,20 +12,18 @@ from PyQt4 import QtCore, QtGui
 class Ui_SteamFriendsStatus(object):
     def setupUi(self, SteamFriendsStatus):
         SteamFriendsStatus.setObjectName("SteamFriendsStatus")
-        SteamFriendsStatus.resize(257, 400)
+        SteamFriendsStatus.resize(257, 360)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(SteamFriendsStatus.sizePolicy().hasHeightForWidth())
         SteamFriendsStatus.setSizePolicy(sizePolicy)
-        self.QuitButton = QtGui.QPushButton(SteamFriendsStatus)
-        self.QuitButton.setGeometry(QtCore.QRect(100, 370, 71, 31))
-        self.QuitButton.setObjectName("QuitButton")
+        SteamFriendsStatus.setMaximumSize(QtCore.QSize(257, 360))
         self.FetchButton = QtGui.QPushButton(SteamFriendsStatus)
-        self.FetchButton.setGeometry(QtCore.QRect(100, 340, 71, 31))
+        self.FetchButton.setGeometry(QtCore.QRect(20, 330, 211, 31))
         self.FetchButton.setObjectName("FetchButton")
         self.SteamUsername = QtGui.QLineEdit(SteamFriendsStatus)
-        self.SteamUsername.setGeometry(QtCore.QRect(1, 27, 256, 23))
+        self.SteamUsername.setGeometry(QtCore.QRect(0, 20, 256, 23))
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -33,13 +31,13 @@ class Ui_SteamFriendsStatus(object):
         self.SteamUsername.setSizePolicy(sizePolicy)
         self.SteamUsername.setObjectName("SteamUsername")
         self.label = QtGui.QLabel(SteamFriendsStatus)
-        self.label.setGeometry(QtCore.QRect(0, 50, 256, 17))
+        self.label.setGeometry(QtCore.QRect(0, 40, 251, 20))
         self.label.setObjectName("label")
         self.LabelSteamID = QtGui.QLabel(SteamFriendsStatus)
         self.LabelSteamID.setGeometry(QtCore.QRect(1, 1, 321, 17))
         self.LabelSteamID.setObjectName("LabelSteamID")
         self.ListSteamFriends = QtGui.QListWidget(SteamFriendsStatus)
-        self.ListSteamFriends.setGeometry(QtCore.QRect(0, 70, 256, 271))
+        self.ListSteamFriends.setGeometry(QtCore.QRect(0, 60, 256, 271))
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -60,37 +58,40 @@ class Ui_SteamFriendsStatus(object):
         self.ListSteamFriends.setViewMode(QtGui.QListView.ListMode)
         self.ListSteamFriends.setUniformItemSizes(False)
         self.ListSteamFriends.setWordWrap(True)
+        self.ListSteamFriends.setSelectionRectVisible(False)
         self.ListSteamFriends.setObjectName("ListSteamFriends")
         QtGui.QListWidgetItem(self.ListSteamFriends)
         self.actionFetchData = QtGui.QAction(SteamFriendsStatus)
         self.actionFetchData.setObjectName("actionFetchData")
         self.actionPopulateListSteamFriends = QtGui.QAction(SteamFriendsStatus)
         self.actionPopulateListSteamFriends.setObjectName("actionPopulateListSteamFriends")
+        self.actionGetServerData = QtGui.QAction(SteamFriendsStatus)
+        self.actionGetServerData.setObjectName("actionGetServerData")
 
         self.retranslateUi(SteamFriendsStatus)
-        QtCore.QObject.connect(self.QuitButton, QtCore.SIGNAL("clicked()"), SteamFriendsStatus.close)
         QtCore.QObject.connect(self.SteamUsername, QtCore.SIGNAL("returnPressed()"), self.FetchButton.click)
-        QtCore.QObject.connect(self.FetchButton, QtCore.SIGNAL("clicked()"), self.ListSteamFriends.clear)
         QtCore.QObject.connect(self.FetchButton, QtCore.SIGNAL("clicked()"), self.actionFetchData.toggle)
+        QtCore.QObject.connect(self.FetchButton, QtCore.SIGNAL("clicked()"), self.ListSteamFriends.clear)
+        QtCore.QObject.connect(self.ListSteamFriends, QtCore.SIGNAL("doubleClicked(QModelIndex)"), self.actionGetServerData.toggle)
         QtCore.QMetaObject.connectSlotsByName(SteamFriendsStatus)
 
     def retranslateUi(self, SteamFriendsStatus):
         SteamFriendsStatus.setWindowTitle(QtGui.QApplication.translate("SteamFriendsStatus", "Steam Friends Status", None, QtGui.QApplication.UnicodeUTF8))
-        self.QuitButton.setText(QtGui.QApplication.translate("SteamFriendsStatus", "Quit", None, QtGui.QApplication.UnicodeUTF8))
         self.FetchButton.setText(QtGui.QApplication.translate("SteamFriendsStatus", "Fetch", None, QtGui.QApplication.UnicodeUTF8))
         self.SteamUsername.setToolTip(QtGui.QApplication.translate("SteamFriendsStatus", "The ID numbers or name can be found by browsing to your Steam Community page and looking in the URL.\n"
 "Short name: http://steamcommunity.com/id/$Name\n"
 "ID number: http://steamcommunity.com/profiles/$Numbers", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setToolTip(QtGui.QApplication.translate("SteamFriendsStatus", "Your online and in-game friends will appear below.", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("SteamFriendsStatus", "Your Friends", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("SteamFriendsStatus", "Friends", None, QtGui.QApplication.UnicodeUTF8))
         self.LabelSteamID.setToolTip(QtGui.QApplication.translate("SteamFriendsStatus", "The ID numbers or name can be found by browsing to your Steam Community page and looking in the URL.\n"
 "Short name: http://steamcommunity.com/id/$Name\n"
 "ID number: http://steamcommunity.com/profiles/$Numbers", None, QtGui.QApplication.UnicodeUTF8))
-        self.LabelSteamID.setText(QtGui.QApplication.translate("SteamFriendsStatus", "Steam Community Identity (ID/Alias)", None, QtGui.QApplication.UnicodeUTF8))
+        self.LabelSteamID.setText(QtGui.QApplication.translate("SteamFriendsStatus", "Steam Community ID/Alias", None, QtGui.QApplication.UnicodeUTF8))
         __sortingEnabled = self.ListSteamFriends.isSortingEnabled()
         self.ListSteamFriends.setSortingEnabled(False)
         self.ListSteamFriends.item(0).setText(QtGui.QApplication.translate("SteamFriendsStatus", "Enter your Steam Community ID above and click fetch!", None, QtGui.QApplication.UnicodeUTF8))
         self.ListSteamFriends.setSortingEnabled(__sortingEnabled)
         self.actionFetchData.setText(QtGui.QApplication.translate("SteamFriendsStatus", "FetchData", None, QtGui.QApplication.UnicodeUTF8))
         self.actionPopulateListSteamFriends.setText(QtGui.QApplication.translate("SteamFriendsStatus", "populateListSteamFriends", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionGetServerData.setText(QtGui.QApplication.translate("SteamFriendsStatus", "GetServerData", None, QtGui.QApplication.UnicodeUTF8))
 
