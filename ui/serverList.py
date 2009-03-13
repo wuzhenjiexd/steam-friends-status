8516e91c3faa33a9d8ed56f710a0d67006e354fb
davidk <kdavid@gmail.com>
2009-03-13 12:49:48 -0700

    Add refresh in the server information window
    Also add some general UI refinements

--- ui/serverList.py
+++ ui/serverList.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'serverList.ui'
 #
-# Created: Thu Mar 12 15:26:11 2009
+# Created: Fri Mar 13 12:47:10 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,10 +13,19 @@ class Ui_serverList(object):
     def setupUi(self, serverList):
         serverList.setObjectName("serverList")
         serverList.resize(428, 466)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(serverList.sizePolicy().hasHeightForWidth())
+        serverList.setSizePolicy(sizePolicy)
+        serverList.setMinimumSize(QtCore.QSize(428, 466))
         serverList.setMaximumSize(QtCore.QSize(428, 466))
         self.serverInfoWidget = QtGui.QListWidget(serverList)
         self.serverInfoWidget.setGeometry(QtCore.QRect(0, 10, 421, 191))
         self.serverInfoWidget.setMaximumSize(QtCore.QSize(421, 191))
+        self.serverInfoWidget.setProperty("isWrapping", QtCore.QVariant(False))
+        self.serverInfoWidget.setResizeMode(QtGui.QListView.Adjust)
+        self.serverInfoWidget.setWordWrap(True)
         self.serverInfoWidget.setObjectName("serverInfoWidget")
         self.tableScores = QtGui.QTableWidget(serverList)
         self.tableScores.setGeometry(QtCore.QRect(0, 210, 421, 221))
@@ -25,11 +34,12 @@ class Ui_serverList(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.tableScores.sizePolicy().hasHeightForWidth())
         self.tableScores.setSizePolicy(sizePolicy)
+        self.tableScores.setMinimumSize(QtCore.QSize(421, 221))
         self.tableScores.setMaximumSize(QtCore.QSize(421, 221))
         self.tableScores.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.tableScores.setAlternatingRowColors(True)
-        self.tableScores.setShowGrid(False)
-        self.tableScores.setWordWrap(False)
+        self.tableScores.setShowGrid(True)
+        self.tableScores.setWordWrap(True)
         self.tableScores.setCornerButtonEnabled(False)
         self.tableScores.setRowCount(0)
         self.tableScores.setColumnCount(3)
@@ -43,8 +53,11 @@ class Ui_serverList(object):
         item = QtGui.QTableWidgetItem()
         self.tableScores.setHorizontalHeaderItem(2, item)
         self.closeButton = QtGui.QPushButton(serverList)
-        self.closeButton.setGeometry(QtCore.QRect(330, 430, 80, 29))
+        self.closeButton.setGeometry(QtCore.QRect(310, 430, 111, 29))
         self.closeButton.setObjectName("closeButton")
+        self.refreshButton = QtGui.QPushButton(serverList)
+        self.refreshButton.setGeometry(QtCore.QRect(110, 430, 191, 29))
+        self.refreshButton.setObjectName("refreshButton")
 
         self.retranslateUi(serverList)
         QtCore.QObject.connect(self.closeButton, QtCore.SIGNAL("clicked()"), serverList.close)
@@ -57,4 +70,5 @@ class Ui_serverList(object):
         self.tableScores.horizontalHeaderItem(1).setText(QtGui.QApplication.translate("serverList", "Score", None, QtGui.QApplication.UnicodeUTF8))
         self.tableScores.horizontalHeaderItem(2).setText(QtGui.QApplication.translate("serverList", "Time", None, QtGui.QApplication.UnicodeUTF8))
         self.closeButton.setText(QtGui.QApplication.translate("serverList", "Close", None, QtGui.QApplication.UnicodeUTF8))
+        self.refreshButton.setText(QtGui.QApplication.translate("serverList", "Refresh", None, QtGui.QApplication.UnicodeUTF8))
 
